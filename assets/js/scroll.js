92f6f6ab52417ca4cff9d09f6d7bc443f1d5c129
favaro.m.john@gmail.com <favaro.m.john@gmail.com>
2018-01-15 23:35:05 -0800

    blah

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -119,7 +119,7 @@
           // offset code to 0 on mobile
           $('html, body').stop().animate({
           
-            'scrollTop': $target.offset().top}, 2000, function () {
+            'scrollTop': $target.offset().top}, 900, function () {
             window.location.hash = target;
            
         });
